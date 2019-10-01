4e80e0007e03919acb5336bcb18c0661dfe44632
mkmrah <39188713+mkmrah@users.noreply.github.com>
2019-10-01 17:33:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+
+variable "test_var" {
+  default = "value"
+  description = "test value"
+}
+
 resource "null_resource" "bash_scrtip_local_exec" {
   provisioner "local-exec" {
     command = "chmod +x test-script.sh | ./test-script.sh"
